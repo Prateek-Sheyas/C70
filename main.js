19603421038b922d632c3c26a0534215b18a96c1
Prateek-Sheyas <116279564+Prateek-Sheyas@users.noreply.github.com>
2023-01-19 18:08:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function schoolLibrary()
+{
+    window.location = "campusLife2.html";
+}
+
+function schoolAuditorium()
+{
+    window.location = "campusLife3.html";
+}
+
+function home()
+{
+    window.location = "index.html";
+}
+
+function schoolAcheivements2()
+{
+    window.location = "schoolAcheivements2";
+}
+
+function schoolAcheivements3()
+{
+    window.location = "schoolAcheivements3";
+}
+
+function whatsNew()
+{
+    window.location = "what'sNew2.html";
+}
